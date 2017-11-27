5a51d29e941d4f8d605358be88df9245b13a0b17
yuqingzou <yuqingzou@users.noreply.github.com>
2017-11-27 17:45:48 -0500

    Update filetopicle.py

--- scripts/filetopicle.py
+++ scripts/filetopicle.py
@@ -18,7 +18,9 @@ def read_input(inputdata = 0, reference = 0, datatype = "DNase-seq", cellline = 
 
     for index, row in df.iterrows():
         if row[1] in chrom_list:
+            print(row[1])
     	    chrom_list[row[1]][row[2]:row[3]] = [1]*(row[3]-row[2])
+            print()
     print(chrom_list['chr1'])
     for lists in chrom_list:   
         with open(datatype+'-'+cellline+'-'+lists+'.pkl', 'wb') as f:
