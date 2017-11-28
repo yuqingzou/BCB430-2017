753d8bbe97fb94464cfdf234530a395b485ae806
yuqingzou <yisubai@icloud.com>
2017-11-28 15:11:32 -0500

    submit sample region code

--- scripts/sample_region.py
+++ scripts/sample_region.py
@@ -1,10 +1,23 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Nov 14 12:11:40 2017
+from random import randint
+import numpy as np
+from random import randint
 
-@author: yisubai
-"""
 
-def sample_region(indir,location,interval):
-    allMat = np.loadtxt(os.path.join(indir, '.pkl'), dtype=str, allow_pickle =True)
+def sample_region(indir,location,interval = 1000):
+	random_chr_num = randint(1, 22)
+	random_chr = np.load('/mnt/raisin/yuqing/collapse/' + 'chr' + random_chr_num + '.dat')
+
+	chr_size = random_chr.shape[1]
+	half = interval/2
+	random_mid_point = randint(half,(chr_size-half))
+
+	sample_array = random_chr[random_mid_point-half:random_mid_point+half]
+
+	return sample_array
+
+def main():
+    sample_region()
+
+
+if __name__ == "__main__":
+    main()
